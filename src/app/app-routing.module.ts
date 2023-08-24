221c3a68521554fbd23211acb810cd02058ddfbe
unknown <latifaj393@gmail.com>
2023-08-24 16:32:24 +0100

    adding admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './component/home/home.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { LoginComponent } from './component/login/login.component';
+import { TeamComponent } from './component/team/team.component';
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path: 'contact', component:ContactComponent},
+  {path:'login',component:LoginComponent},
+  {path:'team',component:TeamComponent},
+  { path: 'admin', loadChildren: () => import('./admin-page/admin-page.module').then(m => m.AdminPageModule) },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
